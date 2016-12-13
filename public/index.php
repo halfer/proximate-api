fd7558ca822e6128daa7e9b48a7804f05f66f990
Jon Hinks <projects.ws@jondh.me.uk>
2016-12-13 22:51:52 +0000

    More notes and planned API endpoints

--- public/index.php
+++ public/index.php
@@ -16,7 +16,7 @@ $app->get('/count', function ($request, $response) {
 /**
  * Counts the number of pages for a specific domain in the cache
  *
- * (Not sure if this is possible)
+ * This information is available from GET "/__admin/mappings"
  */
 $app->get('/count/:url', function ($request, $response) {
     $response->write("Count total pages at this URL base");
@@ -25,6 +25,8 @@ $app->get('/count/:url', function ($request, $response) {
 
 /**
  * List the pages in the cache, given a page and a page size
+ *
+ * This can use GET "/__admin/mappings" from the WireMock playback instance
  */
 $app->get('/list/[{page}]/[{pagesize}]', function ($request, $response) {
     $response->write("List pages");
@@ -33,14 +35,23 @@ $app->get('/list/[{page}]/[{pagesize}]', function ($request, $response) {
 
 /**
  * Requests that a specific site is cached
+ *
+ * Adds the item onto the queueing system and returns a unique GUID
  */
 $app->post('/cache/{url}', function ($request, $response, $args) {
     $response->write("Cache the specified page");
     return $response;
 });
 
+$app->get('/status/{guid}', function ($request, $response, $args) {
+    $response->write("Determines the status of the specified item");
+    return $response;
+});
+
 /**
  * Requests that a specific site is deleted from the cache
+ *
+ * This can use DELETE "/__admin/mappings/{stubMappingId}" from the WireMock playback instance
  */
 $app->delete('/cache/{url}', function ($request, $response, $args) {
     $response->write("Delete the specified page");
