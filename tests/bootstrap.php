85e12a34a35c9c9d8933dbed80f810dc7483bbf6
Jon Hinks <projects.ws@jondh.me.uk>
2016-12-17 11:54:24 +0000

    Add test skeleton class

---
+++ tests/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+$root = realpath(__DIR__ . '/..');
+require_once $root . '/vendor/autoload.php';
+require_once $root . '/src/autoload.php';
---
+++ tests/unit/QueueTest.php
@@ -0,0 +1,13 @@
+<?php
+
+/** 
+ * Unit tests for the Queue
+ */
+
+class QueueTest extends PHPUnit_Framework_TestCase
+{
+    public function testCase()
+    {
+        $this->assertTrue(true);
+    }
+}
