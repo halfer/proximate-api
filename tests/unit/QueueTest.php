0488322b18b03124bc7ddde33244578a2d039562
Jon Hinks <projects.ws@jondh.me.uk>
2016-12-17 12:01:10 +0000

    Add some suggested unit tests for the queue

--- tests/unit/QueueTest.php
+++ tests/unit/QueueTest.php
@@ -6,8 +6,58 @@
 
 class QueueTest extends PHPUnit_Framework_TestCase
 {
-    public function testCase()
+    public function testConstructorStoresDirectory()
     {
-        $this->assertTrue(true);
+        $this->markTestIncomplete();
+    }
+
+    public function testConstructorAllowsGoodFolder()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testConstructorRejectsBadFolder()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testUrlStorage()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetUrlFailsWithNoUrl()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testUrlRegexStorage()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testRejectFilesStorage()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testNewQueueItemSucceeds()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testExistingQueueItemFails()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testProcessor()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function checkProcessorCallsSleep()
+    {
+        $this->markTestIncomplete();
     }
 }
