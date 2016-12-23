bd47ae108dc82437c39870b5c0f9ecba9d411fbf
Jon Hinks <projects.ws@jondh.me.uk>
2016-12-23 18:32:01 +0000

    Repair broken class reference

--- src/processes/queue.php
+++ src/processes/queue.php
@@ -4,11 +4,16 @@
  * Launches the queue, will be restarted by Supervisor when it finishes
  */
 
-use \Proximate\Service\File;
+use Proximate\Service\File;
+use Proximate\Service\SiteFetcher;
 
 $root = realpath(__DIR__ . '/../..');
 require_once $root . '/vendor/autoload.php';
 require_once $root . '/src/autoload.php';
 
-$queue = new Proximate\Queue('/var/proximate/queue', new File());
-$queue->processor();
+$queue = new Proximate\Queue\Read(
+    '/var/proximate/queue',
+    new File(),
+    new SiteFetcher()
+);
+$queue->process();
