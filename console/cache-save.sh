83ca9ed0d3930712bc7d22081331b846b071fecd
Jon Hinks <projects.ws@jondh.me.uk>
2016-12-23 20:58:50 +0000

    Add some e2e test scripts

---
+++ console/cache-save.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Adds a new cache requirement to the queue
+
+JSON=`php -r "echo json_encode(['url' => 'http://www.nimvelo.com/about/careers/', 'url_regex' => '.*(/about/careers/.*)|(/job/.*)']);"`
+echo "Input doc: $JSON"
+curl \
+	--data $JSON \
+	http://localhost:8080/cache
+echo
---
+++ console/count.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Runs the count endpoint in the API
+
+curl http://localhost:8080/count
+echo
