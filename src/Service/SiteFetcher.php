5957e34fd34db314dd92c89c1cd8c052848de6ca
Jon Hinks <projects.ws@jondh.me.uk>
2017-04-20 18:57:55 +0100

    WIP converting SiteFetcher to use new crawler

--- src/Service/SiteFetcher.php
+++ src/Service/SiteFetcher.php
@@ -7,55 +7,27 @@
 namespace Proximate\Service;
 
 use Proximate\Exception\SiteFetch as SiteFetchException;
+use Proximate\SimpleCrawler;
 
 class SiteFetcher
 {
-    protected $httpProxy;
-    protected $httpsProxy;
+    protected $proxyAddress;
     protected $lastLog;
 
-    public function __construct($httpProxy, $httpsProxy)
+    public function __construct($proxyAddress)
     {
-        $this->setProxies($httpProxy, $httpsProxy);
+        $this->setProxyAddress($proxyAddress);
     }
 
-    public function execute($url, $urlRegex, $rejectFiles)
+    public function execute($startUrl, $pathRegex)
     {
-        // Here are some optional parameters
-        $rejectFilesCmd = $rejectFiles ?
-            "--reject \"{$rejectFiles}\"" :
-            '';
-        $urlRegexCmd = $urlRegex ?
-            "--accept-regex \"{$urlRegex}\"" :
-            '';
+        $crawler = new SimpleCrawler($this->proxyAddress);
+        $crawler->
+            init()->
+            crawl($startUrl, $pathRegex);
 
-        // Construct a site fetch command.
-        //
-        // output-file: redirects verbose log output. Use /dev/stdout to capture it in PHP
-        // directory-prefix: redirect file operations to a writeable location
-        // delete-after: the saved files will be deleted when wget finishes
-        // no-directories: turns off the hierarchical directory-based save format
-        $raw = "
-            wget \\
-                --output-file /tmp/wget.log \\
-                --directory-prefix=/tmp/wget/ \\
-                --recursive \\
-                --wait 3 \\
-                --limit-rate=20K \\
-                --delete-after \\
-                --no-directories \\
-                {$rejectFilesCmd} \\
-                {$urlRegexCmd} \\
-                -e use_proxy=yes \\
-                -e http_proxy={$this->httpProxy} \\
-                -e https_proxy={$this->httpsProxy} \\
-                {$url}
-        ";
-        $skipLines = $this->trimBlankLines($raw);
-        $command = rtrim($skipLines);
-
-        $ok = $this->runCommand($command);
-        if (!$ok)
+        // FIXME how do we know it failed?
+        if (false)
         {
             throw new SiteFetchException(
                 "There was a problem with the site fetch call"
@@ -66,53 +38,10 @@ class SiteFetcher
     /**
      * Useful for testing (when the constructor is not called)
      *
-     * @param string $httpProxy
-     * @param string $httpsProxy
+     * @param string $proxyAddress
      */
-    public function setProxies($httpProxy, $httpsProxy)
+    public function setProxyAddress($proxyAddress)
     {
-        $this->httpProxy = $httpProxy;
-        $this->httpsProxy = $httpsProxy;
-    }
-
-    /**
-     * Removes empty command lines just containing a backslash and whitespace
-     *
-     * @param string $text
-     * @return string
-     */
-    protected function trimBlankLines($text)
-    {
-        return array_reduce(
-            explode("\n", $text),
-            function($output, $line) {
-                if (trim($line) != '\\') {
-                    $output .= $line . "\n";
-                }
-                return $output;
-            },
-            ''
-        );
-    }
-
-    /**
-     * Runs a system command
-     *
-     * Note that the log output needs to be sent to /dev/stdout for the output logger to work
-     *
-     * @param string $command
-     * @return boolean
-     */
-    protected function runCommand($command)
-    {
-        $this->lastLog = $return = null;
-        exec($command, $this->lastLog, $return);
-
-        return $return === 0;
-    }
-
-    public function getLastLog()
-    {
-        return $this->lastLog;
+        $this->proxyAddress = $proxyAddress;
     }
 }
