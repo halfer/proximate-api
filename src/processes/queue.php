3f5a155f298e33a1f867f2b350956c247dcc28fc
Jon Hinks <projects.ws@jondh.me.uk>
2017-04-23 22:38:04 +0100

    Pass the queue path to the queue process script

--- src/processes/queue.php
+++ src/processes/queue.php
@@ -1,25 +1,38 @@
+#!/usr/bin/env php
 <?php
 
 /**
  * Launches the queue, will be restarted by Supervisor when it finishes
  *
- * @todo How can I test whether the below code works? e.g. if the constructors correct?
- * @todo Might be worth sending the proxy addresses in via an env var, and reading it here?
+ * @todo Create an integration test for the queue
+ * @todo Might be worth sending the proxy address in via an env var, and reading it here?
  */
 
 use Proximate\Service\File;
 use Proximate\Service\SiteFetcher as SiteFetcherService;
+use Proximate\Queue\Read as QueueReader;
 
 $root = realpath(__DIR__ . '/../..');
 require_once $root . '/vendor/autoload.php';
 require_once $root . '/src/autoload.php';
 
-$curlApi = new PestJSON('http://proximate-proxy:8083');
-$curlPlayback = new PestJSON('http://proximate-proxy:8082');
-$queue = new Proximate\Queue\Read(
-    '/var/proximate/queue',
-    new File()
-);
+if ($argc != 2)
+{
+    $command = __FILE__;
+    die(
+        sprintf("Syntax: %s <queue-path>\n", $command)
+    );
+}
+
+$queuePath = $argv[1];
+if (!file_exists($queuePath))
+{
+    die(
+        sprintf("Error: the supplied queue path `%s` does not exist\n", $queuePath)
+    );
+}
+
+$queue = new QueueReader($queuePath, new File());
 $queue->
     setFetcher(new SiteFetcherService('proximate-proxy:8081'))->
     process();
