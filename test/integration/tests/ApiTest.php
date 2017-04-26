22c5e77e65dee6ce6db21af101e8e37d7d4c489a
Jon Hinks <projects.ws@jondh.me.uk>
2017-04-26 12:09:29 +0100

    Add skeleton integration tests for API

---
+++ test/integration/tests/ApiTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Functional test for API
+ */
+
+namespace Proximate\Test\Integration;
+
+use halfer\SpiderlingUtils\TestCase;
+
+class ApiTest extends TestCase
+{
+    /**
+     * @todo Reset the queue to ensure tests do not become serial
+     */
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function testNonExistentEndpoint()
+    {
+        // @todo
+        $this->markTestIncomplete();
+    }
+
+    public function testFetchPage()
+    {
+        // @todo
+        $this->markTestIncomplete();
+    }
+
+    public function testFetchCount()
+    {
+        // @todo
+        $this->markTestIncomplete();
+    }
+
+    public function testFetchCacheItem()
+    {
+        // @todo
+        $this->markTestIncomplete();
+    }
+
+    public function testDeleteCacheItem()
+    {
+        // @todo
+        $this->markTestIncomplete();
+    }
+
+    public function testQueueItem()
+    {
+        // @todo
+        $this->markTestIncomplete();
+    }
+}
