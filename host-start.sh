534c87545baf66d93255cec376da13060431a009
Jon Hinks <projects.ws@jondh.me.uk>
2017-05-09 15:27:20 +0100

    Fix space consistency in launch script

--- host-start.sh
+++ host-start.sh
@@ -8,9 +8,9 @@ ROOTDIR=`pwd`
 docker run \
         -p 127.0.0.1:8080:8080 \
         -v ${ROOTDIR}/cache:/remote/cache \
-		-v /etc/localtime:/etc/localtime:ro \
-		-v /etc/timezone:/etc/timezone:ro \
-		-e PHP_TIMEZONE=Europe/London \
+        -v /etc/localtime:/etc/localtime:ro \
+        -v /etc/timezone:/etc/timezone:ro \
+        -e PHP_TIMEZONE=Europe/London \
         -t \
         proximate-api
 
