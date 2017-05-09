facb8bdd10ecec104c5afce94cc6e0143391b01c
Jon Hinks <projects.ws@jondh.me.uk>
2017-05-09 21:38:35 +0100

    Fix whitespace consistency in shell script

--- host-start.sh
+++ host-start.sh
@@ -6,13 +6,13 @@ cd `dirname $0`
 ROOTDIR=`pwd`
 
 docker run \
-        -p 127.0.0.1:8080:8080 \
-        -v ${ROOTDIR}/cache:/remote/cache \
-        -v /etc/localtime:/etc/localtime:ro \
-        -v /etc/timezone:/etc/timezone:ro \
-        -e PHP_TIMEZONE=Europe/London \
-        -t \
-        proximate-api
+    -p 127.0.0.1:8080:8080 \
+    -v ${ROOTDIR}/cache:/remote/cache \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /etc/timezone:/etc/timezone:ro \
+    -e PHP_TIMEZONE=Europe/London \
+    -t \
+    proximate-api
 
 # Go back to original dir
 cd $STARTDIR
